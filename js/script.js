dce19ab999f501c8a3250e6c980655d5e4086e43
KasiMaru <timurkasievsochi@gmail.com>
2017-09-08 10:45:42 +0300

    Правки от проверяющего

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-//Открывалка каталога//
-
-var catalogOpener = document.querySelector('.catalog-opener');
-var catalogList = document.querySelector('.catalog-list');
-
-catalogOpener.addEventListener('click', function () {
-  catalogList.classList.toggle('hidden');
-});
-
-catalogOpener.addEventListener('keypress', function (press) {
-  if (press.keyCode == '13') {
-    catalogList.classList.toggle('hidden');
-  }
-});
-
 
 //Логин-логаут//
 
